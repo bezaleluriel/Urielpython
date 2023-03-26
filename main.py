12462bed264b6674ff0a5f20d77c66ee51af0d0c
bezaleluriel <bezaleluriel@gmail.com>
2023-03-26 10:29:44 +0300

    bla

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    x = input("enter your full name: ")
+    first_name = x.split()[0]
+    last_name = x.split()[1]
+    first_name = first_name[0].upper() + first_name[1:]
+    last_name = last_name[0].upper() + last_name[1:]
+    print(f'{first_name} {last_name}')
+
+
+
+
+
+
+    x = input('enter text: ')
+    print(x == x[::-1])
+    print(type(x))
+    print(x.upper())
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
