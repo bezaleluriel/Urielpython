c7d7bd692f680bbb0981632189788428acf2d0be
bezaleluriel <bezaleluriel@gmail.com>
2023-05-04 09:10:18 +0300

    GUI

--- GUI_EX/main.py
+++ GUI_EX/main.py
@@ -1,6 +1,22 @@
 from PyQt5 import  QtWidgets
-app = QtWidgets.QApplication([])
-w = QtWidgets.QWidget()
-w.show()
-# make sure the window will not close
+from PyQt5.QtWidgets import QDialog,QApplication
+from PyQt5.uic import loadUi
+
+
+class WelcomeScreen(QDialog):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi(r'DesignerScreens\login.ui',self)
+
+app = QApplication([])
+welcome = WelcomeScreen()
+welcome.show()
 app.exec_()
+
+
+
+# app = QtWidgets.QApplication([])
+# w = QtWidgets.QWidget()
+# w.show()
+# # make sure the window will not close
+# app.exec_()
